29ab7629d1ef9380e4f3fe3add960ab18b62a1a3
d41dev <d41dev@proton.me>
2025-09-10 23:22:29 +0100

    created classify function

--- processors/FewShotClassifier.kt
+++ processors/FewShotClassifier.kt
@@ -8,62 +8,35 @@ import com.fpf.smartscansdk.clip.getTopN
 import com.fpf.smartscansdk.utils.getBitmapFromUri
 import com.fpf.smartscansdk.clip.PrototypeEmbedding
 
-class FewShotClassifier(
-    private val context: Context,
-    private val embedder: Embedder,
-    private val prototypeList: List<PrototypeEmbedding>,
-    ): IProcessor<String, ClassificationResult> {
+suspend fun classify(context: Context, imageUri: String,embedder: Embedder, prototypeList: List<PrototypeEmbedding>,): ClassificationResult{
+    val bitmap = getBitmapFromUri(context, imageUri.toUri())
+    val imageEmbedding = embedder.generateImageEmbedding(bitmap)
+    bitmap.recycle()
 
-    companion object {
-        const val TAG = "FewShotClassifier"
-    }
+    val similarities = getSimilarities(imageEmbedding, prototypeList.map { it.embeddings })
+    val top2 = getTopN(similarities, 2)
 
-    override suspend fun onProgress(processedCount: Int, total: Int) {
-        TODO("Not yet implemented")
-    }
+    if(top2.isEmpty()) return ClassificationResult.Failure(itemId = imageUri)
 
-    override suspend fun onComplete(context: Context, totalProcessed: Int, processingTime: Long
-    ) {
-        TODO("Not yet implemented")
-    }
+    val bestIndex = top2[0]
+    val bestSim = similarities[bestIndex]
+    val secondSim = top2.getOrNull(1)?.let { similarities[it] } ?: 0f
 
-    override suspend fun onBatchComplete(context: Context, batch: List<ClassificationResult>): List<ClassificationResult> {
-        return batch
-    }
+    val threshold = 0.4f
+    val minMargin = 0.05f
 
-    override suspend fun onProcess(context: Context, imageUri: String): ClassificationResult{
-        val bitmap = getBitmapFromUri(context, imageUri.toUri())
-        val imageEmbedding = embedder.generateImageEmbedding(bitmap)
-        bitmap.recycle()
+    if (bestSim < threshold) return ClassificationResult.Failure(itemId = imageUri) // don't move if below threshold
+    if((bestSim - secondSim) < minMargin) return ClassificationResult.Failure(itemId = imageUri) // don't move if gap between best and second is too small
 
-        val similarities = getSimilarities(imageEmbedding, prototypeList.map { it.embeddings })
-        val top2 = getTopN(similarities, 2)
+    val classId = prototypeList.getOrNull(bestIndex)?.id
 
-        if(top2.isEmpty()) return ClassificationResult.Failure(itemId = imageUri)
-
-        val bestIndex = top2[0]
-        val bestSim = similarities[bestIndex]
-        val secondSim = top2.getOrNull(1)?.let { similarities[it] } ?: 0f
-
-        val threshold = 0.4f
-        val minMargin = 0.05f
-
-        if (bestSim < threshold) return ClassificationResult.Failure(itemId = imageUri) // don't move if below threshold
-        if((bestSim - secondSim) < minMargin) return ClassificationResult.Failure(itemId = imageUri) // don't move if gap between best and second is too small
-
-        val classId = prototypeList.getOrNull(bestIndex)?.id
-
-        return if (classId == null) {
-            ClassificationResult.Failure(itemId = imageUri)
-        } else {
-            ClassificationResult.Success(itemId = imageUri, classId)
-        }
-    }
-
-    fun close() {
-        embedder.closeSession()
+    return if (classId == null) {
+        ClassificationResult.Failure(itemId = imageUri)
+    } else {
+        ClassificationResult.Success(itemId = imageUri, classId)
     }
 }
+
 
 sealed class ClassificationResult {
     data class Success(val itemId: String, val classId: String) : ClassificationResult()
