ea79aced84761332819c8742b770d3da1645741c
d41dev <d41dev@proton.me>
2025-09-12 14:07:34 +0100

    add FileEmbeddingRetriever impl

--- extensions/src/main/java/com/fpf/smartscansdk/extensions/embeddings/FileEmbeddingRetriever.kt
+++ extensions/src/main/java/com/fpf/smartscansdk/extensions/embeddings/FileEmbeddingRetriever.kt
@@ -1,4 +1,33 @@
 package com.fpf.smartscansdk.extensions.embeddings
 
-class FileEmbeddingRetriever {
+import com.fpf.smartscansdk.core.ml.embeddings.Embedding
+import com.fpf.smartscansdk.core.ml.embeddings.IRetriever
+import com.fpf.smartscansdk.core.ml.embeddings.getSimilarities
+import com.fpf.smartscansdk.core.ml.embeddings.getTopN
+
+class FileEmbeddingRetriever(
+    private val store: FileEmbeddingStore
+): IRetriever {
+    override suspend fun query(
+        embedding: FloatArray,
+        topK: Int,
+        threshold: Float
+    ): List<Embedding> {
+
+        val storedEmbeddings = store.load()
+
+        if (storedEmbeddings.isEmpty()) {
+            return emptyList()
+        }
+
+        val similarities = getSimilarities(embedding, storedEmbeddings.map { it.embeddings })
+        val results = getTopN(similarities, topK, threshold)
+
+        if (results.isEmpty()) {
+            return emptyList()
+        }
+
+        return results.map{idx -> storedEmbeddings[idx]}
+    }
 }
+
