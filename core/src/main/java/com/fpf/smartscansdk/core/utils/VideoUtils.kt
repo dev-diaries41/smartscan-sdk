dc3d78bf70152cc6c92c1963ea14b0c977ecb0af
d41dev <d41dev@proton.me>
2025-09-13 11:19:18 +0100

    move video frame extract to VideoUtils

--- core/src/main/java/com/fpf/smartscansdk/core/utils/VideoUtils.kt
+++ core/src/main/java/com/fpf/smartscansdk/core/utils/VideoUtils.kt
@@ -1,46 +1,45 @@
 package com.fpf.smartscansdk.core.utils
 
-import android.content.ContentUris
 import android.content.Context
 import android.graphics.Bitmap
 import android.media.MediaMetadataRetriever
 import android.net.Uri
-import android.provider.MediaStore
-import androidx.core.graphics.scale
+import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-fun getVideoUriFromId(id: Long): Uri {
-    return ContentUris.withAppendedId(
-        MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
-        id
-    )
-}
+suspend fun extractFramesFromVideo(context: Context, videoUri: Uri, width: Int, height: Int,frameCount: Int = 10): List<Bitmap>? =withContext(
+    Dispatchers.IO) {
+    val retriever = MediaMetadataRetriever()
+    return@withContext try {
+        retriever.setDataSource(context, videoUri)
 
-suspend fun loadVideoThumbnailFromUri(
-    context: Context,
-    uri: Uri,
-    maxSize: Int
-): Bitmap? {
-    return withContext(Dispatchers.IO) {
-        BitmapCache.get(uri) ?: try {
-            val retriever = MediaMetadataRetriever()
-            retriever.setDataSource(context, uri)
-            var bitmap = retriever.getFrameAtTime(0, MediaMetadataRetriever.OPTION_CLOSEST_SYNC)
-            retriever.release()
+        val durationUs = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)?.toLong()?.times(1000)
+            ?: return@withContext null
+
+        val frameList = mutableListOf<Bitmap>()
+
+        for (i in 0 until frameCount) {
+            val frameTimeUs = (i * durationUs) / frameCount
+            val bitmap = retriever.getScaledFrameAtTime(frameTimeUs, MediaMetadataRetriever.OPTION_CLOSEST_SYNC,
+                width, height
+            )
+
             if (bitmap != null) {
-                val (w, h) = getScaledDimensions(
-                    imgWith = bitmap.width,
-                    imgHeight = bitmap.height,
-                    maxSize
-                )
-                bitmap = bitmap.scale(w, h)
+                frameList.add(bitmap)
+            } else {
+                // Temporary Fix: Break early if null which suggest codec issue with video
+                break
             }
-            bitmap?.let { BitmapCache.put(uri, it) }
-            bitmap
-        } catch (e: Exception) {
-            e.printStackTrace()
-            null
         }
+
+        if (frameList.isEmpty()) return@withContext  null
+
+        frameList
+    } catch (e: Exception) {
+        Log.e("VideoUtils", "Error extracting frames", e)
+        null
+    } finally {
+        retriever.release()
     }
 }
