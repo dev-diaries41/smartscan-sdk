2cccaa6047dd6a2f3b890ac49c8177ea650bde8d
d41dev <d41dev@proton.me>
2025-09-14 11:48:17 +0100

    refator classify

--- core/src/main/java/com/fpf/smartscansdk/core/ml/embeddings/FewShotClassifier.kt
+++ core/src/main/java/com/fpf/smartscansdk/core/ml/embeddings/FewShotClassifier.kt
@@ -1,40 +1,18 @@
 package com.fpf.smartscansdk.core.ml.embeddings
 
-import android.content.Context
-import androidx.core.net.toUri
-import com.fpf.smartscansdk.core.utils.getBitmapFromUri
-
-suspend fun classify(context: Context, imageUri: String, embedder: ImageEmbeddingProvider, prototypeList: List<PrototypeEmbedding>, maxSize: Int): ClassificationResult{
-    val bitmap = getBitmapFromUri(context, imageUri.toUri(), maxSize)
-    val imageEmbedding = embedder.embed(bitmap)
-    bitmap.recycle()
-
-    val similarities = getSimilarities(imageEmbedding, prototypeList.map { it.embeddings })
+fun classify(embedding: FloatArray, prototypes: List<PrototypeEmbedding>, threshold: Float = 0.4f, minMargin: Float = 0.05f, ): String?{
+    val similarities = getSimilarities(embedding, prototypes.map { it.embeddings })
     val top2 = getTopN(similarities, 2)
 
-    if(top2.isEmpty()) return ClassificationResult.Failure(itemId = imageUri)
+    if(top2.isEmpty()) return null
 
     val bestIndex = top2[0]
     val bestSim = similarities[bestIndex]
     val secondSim = top2.getOrNull(1)?.let { similarities[it] } ?: 0f
 
-    val threshold = 0.4f
-    val minMargin = 0.05f
+    if (bestSim < threshold) return null
+    if((bestSim - secondSim) < minMargin) return null //inconclusive -  gap between best and second is too small
 
-    if (bestSim < threshold) return ClassificationResult.Failure(itemId = imageUri) // don't move if below threshold
-    if((bestSim - secondSim) < minMargin) return ClassificationResult.Failure(itemId = imageUri) // don't move if gap between best and second is too small
-
-    val classId = prototypeList.getOrNull(bestIndex)?.id
-
-    return if (classId == null) {
-        ClassificationResult.Failure(itemId = imageUri)
-    } else {
-        ClassificationResult.Success(itemId = imageUri, classId)
-    }
-}
-
-
-sealed class ClassificationResult {
-    data class Success(val itemId: String, val classId: String) : ClassificationResult()
-    data class Failure(val itemId: String) : ClassificationResult()
+    val classId = prototypes.getOrNull(bestIndex)?.id
+    return classId
 }
