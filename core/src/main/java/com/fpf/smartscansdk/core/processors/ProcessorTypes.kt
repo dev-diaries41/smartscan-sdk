b789702ebfe34c6515466769a4fe64bb8c51e6cf
d41dev <d41dev@proton.me>
2025-09-16 13:54:52 +0100

    update: rename generic names for clarity

--- core/src/main/java/com/fpf/smartscansdk/core/processors/ProcessorTypes.kt
+++ core/src/main/java/com/fpf/smartscansdk/core/processors/ProcessorTypes.kt
@@ -16,11 +16,11 @@ data class ProcessOptions(
     val batchSize: Int = 10
 )
 
-interface IProcessorListener<TInput, TOutput> {
+interface IProcessorListener<Input, Output> {
     suspend fun onActive(context: Context) = Unit
-    suspend fun onBatchComplete(context: Context, batch: List<TOutput>) = Unit
+    suspend fun onBatchComplete(context: Context, batch: List<Output>) = Unit
     suspend fun onComplete(context: Context, metrics: Metrics.Success) = Unit
     suspend fun onProgress(context: Context, progress: Float) = Unit
-    fun onError(context: Context, error: Exception, item: TInput) = Unit
+    fun onError(context: Context, error: Exception, item: Input) = Unit
     suspend fun onFail(context: Context, failureMetrics: Metrics.Failure) = Unit
 }
