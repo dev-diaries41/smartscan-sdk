67905bce15d2ed188265dcddfc9f6394ab78c061
d41dev <d41dev@proton.me>
2025-09-16 07:44:28 +0100

    add method to clear all

--- extensions/src/androidTest/kotlin/com/fpf/smartscansdk/extensions/data/images/Dao.kt
+++ extensions/src/androidTest/kotlin/com/fpf/smartscansdk/extensions/data/images/Dao.kt
@@ -8,6 +8,9 @@ interface ImageEmbeddingDao {
     @Query("SELECT * FROM image_embeddings ORDER BY date DESC")
     suspend fun getAllEmbeddingsSync(): List<ImageEmbeddingEntity>
 
+    @Query("DELETE FROM image_embeddings")
+    suspend fun deleteAll()
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertImageEmbedding(imageEmbedding: ImageEmbeddingEntity)
 
